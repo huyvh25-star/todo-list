a7b060e4865a6e78dc2a1b93a9a9342fee038602
vu hoang huy <huyvh25@gmail.com>
2025-10-08 23:12:29 +0700

    done project

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,28 @@ import taskRouters from "./routes/taskRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const app = express();
-
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 // midware
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 // router
 app.use("/api/tasks", taskRouters);
-const PORT = process.env.PORT || 5001;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../fontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../fontend/dist/index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("server đang chạy Port : ", PORT);
